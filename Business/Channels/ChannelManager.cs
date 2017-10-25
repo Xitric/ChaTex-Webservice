f18c1761b719bc0d2c7a899a77def332f358ad3d
Xitric <KasperSchultzDavidsen@gmail.com>
2017-10-25 17:18:17 +0200

    Service for creating a channel

---
+++ Business/Channels/ChannelManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Models;
+
+namespace Business.Channels
+{
+    class ChannelManager : IChannelManager
+    {
+        private readonly IChannelRepository channelRepository;
+        private readonly IGroupRepository groupRepository;
+
+        public ChannelManager(IChannelRepository channelRepository, IGroupRepository groupRepository)
+        {
+            this.channelRepository = channelRepository;
+            this.groupRepository = groupRepository;
+        }
+
+        public bool CreateChannel(int groupId, int callerId, string channelName)
+        {
+            GroupUserModel user = groupRepository.GetGroupUser(groupId, callerId);
+
+            if (user.IsAdministrator)
+            {
+                channelRepository.CreateChannel(groupId, channelName);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ DAL/ChannelRepository.cs
@@ -0,0 +1,23 @@
+﻿using Business;
+using DAL.Models;
+
+namespace DAL
+{
+    class ChannelRepository : IChannelRepository
+    {
+        public void CreateChannel(int groupId, string name)
+        {
+            using (var db = new ChatexdbContext())
+            {
+                Channel channel = new Channel()
+                {
+                    GroupId = groupId,
+                    Name = name
+                };
+
+                db.Channel.Add(channel);
+                db.SaveChanges();
+            }
+        }
+    }
+}
