20099063750583b8d0c3eed4b988654ccb8ccbff
Xitric <KasperSchultzDavidsen@gmail.com>
2017-12-02 00:31:52 +0100

    Fixed the web service so it runs on Azure

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -8,6 +8,7 @@ namespace WebAPI
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseApplicationInsights()
                 .UseKestrel()
                 .UseUrls("http://+:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
